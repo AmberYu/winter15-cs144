f5ccf20d2f8092fcc9f3ffcb1d841dd33c4ee8d8
Amber Yu <yujingzhi91@gmail.com>
2015-02-23 00:37:33 -0800

    ProxyServlet.java compile successful

--- project4/src/edu/ucla/cs/cs144/ProxyServlet.java
+++ project4/src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -1,6 +1,10 @@
 package edu.ucla.cs.cs144;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.net.HttpURLConnection;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
@@ -15,5 +19,25 @@ public class ProxyServlet extends HttpServlet implements Servlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         // your codes here
+        response.setContentType("text/xml");
+        //get query
+        String q = request.getParameter("q");
+        //URL encoding of query string parameters
+        URL url = new URL("http://google.com/complete/search?output=toolbar&q="+URLEncoder.encode(q, "UTF-8"));
+        
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.connect();
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        String suggest = "";
+        while((inputLine = in.readLine())!=null){
+            suggest += inputLine;
+        }
+        
+        in.close();
+        connection.disconnect();
+        
+        response.getWriter().println(suggest);
     }
 }
